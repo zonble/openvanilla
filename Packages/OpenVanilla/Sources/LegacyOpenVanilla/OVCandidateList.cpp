2dedb0556bf55b21ada9d279e79d80cf8e9405e5
zonble <zonble@gmail.com>
2024-10-08 16:21:20 +0800

    Suppresses warnings.

--- Packages/OpenVanilla/Sources/LegacyOpenVanilla/OVCandidateList.cpp
+++ Packages/OpenVanilla/Sources/LegacyOpenVanilla/OVCandidateList.cpp
@@ -69,11 +69,8 @@ void OVCandidateList::update(OVCandidate *textbar)
     if (perpage > 0) {
         int totalpage = (count % perpage) ? (count/perpage)+1 : (count/perpage);
         int currentpage=(pos/perpage)+1;
-    //  if (totalpage >1)
-    //  {
-            sprintf (buf, "(%d/%d)", currentpage, totalpage);
-            textbar->append(buf);
-    //  }
+        sprintf (buf, "(%d/%d)", currentpage, totalpage);
+        textbar->append(buf);
     }
     textbar->update();
 }
